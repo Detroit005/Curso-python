bcbd29c80477ccf4e83e62246290ef671261b294
Detroit005 <diogopalmeida01@gmail.com>
2025-11-05 21:42:52 -0300

    BhaskaraNovo

--- aula06/baskaraST.py
+++ aula06/baskaraST.py
@@ -1,29 +1,34 @@
-import streamlit as st 
-import math as mt
-st.set_page_config(page_title = 'Bhaskara Calculator')
-st.header('Calculadora Bhaskara')
-st.write('Calculadora de raízes \n de uma equação de 2º grau')
-st.write('ax² + bx + c = 0')
+from streamlit import header,write,text_input,button,warning,error,set_page_config
+from math import sqrt,pow
+set_page_config(page_title = 'Bhaskara Calculator')
+# Função python
+def calculo(delta):
+    valor = (sqrt(delta)) / 2 * a
+    return valor
+header('Calculadora Bhaskara')
+write('Calculadora de raízes \n de uma equação de 2º grau')
+write('ax² + bx + c = 0')
 # Entrada
-a = st.text_input('Digite o valor de a:')
-b = st.text_input('Digite o valor de b:')
-c = st.text_input('Digite o valor de c:')
+a = text_input(f'Digite o valor de a:')
+b = text_input(f'Digite o valor de b:')
+c = text_input(f'Digite o valor de c:')
 # Processamento
-if st.button('Calcular raízes'):
+if button('Calcular raízes'):
     try:
         a = float(a)
         b = float(b)
         c = float(c)
-        delta = mt.pow(b,2) - 4 * a * c
+        delta = pow(b,2) - 4 * a * c
         if delta < 0:
-            st.warning('A equação não possui raízes reais')
+            warning('A equação não possui raízes reais')
         elif delta == 0:
-            raiz= (-b + mt.sqrt(delta)) / (2*a)
-            st.write(f'A equação possui uma raiz real: {raiz}')
+            raiz= (-b + calculo(delta))
+            write(f'A equação possui uma raiz real: {raiz}')
         else:
-            raiz1= (-b + mt.sqrt(delta)) / (2*a)
-            raiz2 = (-b - mt.sqrt(delta)) / (2*a)
-            st.write(f'As raízes da equação são: \n Raíz-1:{raiz1:.2f} \n Raíz-2: {raiz2:.2f}')
-    except:
-        st.error('Por favor, insira valores validos para a, b e c')
-st.write('Valor de delta é: {delta}')
+            raiz1= (-b + calculo(delta))
+            raiz2 = (-b - calculo(delta))
+            write(f'As raízes da equação são: \n Raíz-1:{raiz1: .2f} \n Raíz-2: {raiz2: .2f}')
+    except ValueError:
+        error('Por favor, insira valores validos para a, b e c')
+    except ZeroDivisionError:
+        error('O valor de "a" não pode ser zero em uma equação de 2º grau')
